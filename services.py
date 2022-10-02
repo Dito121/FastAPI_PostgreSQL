a15b98a02b3828650c029baa6f13617eed15fe8f
Gerardo Ayes <gerardoayes92@gmail.com>
2022-10-02 14:35:44 -0400

    full CRUD functionality in place, need to automate tests next

--- services.py
+++ services.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 import database as _database
 import models as _models
 import schemas as _schemas
@@ -26,4 +26,32 @@ async def create_contact(
     db.add(contact)
     db.commit()
     db.refresh(contact)
+    return _schemas.Contact.from_orm(contact)
+
+
+async def get_all_contacts(db: "Session") -> List[_schemas.Contact]:
+    contacts = db.query(_models.Contact).all()
+    return list(map(_schemas.Contact.from_orm, contacts))
+
+
+async def get_contact(contact_id: int, db: "Session") -> _schemas.Contact:
+    return db.query(_models.Contact).filter(_models.Contact.id == contact_id).first()
+
+
+async def delete_contact(contact: _models.Contact, db: "Session"):
+    db.delete(contact)
+    db.commit()
+
+
+async def update_contact(
+    contact_data: _schemas.CreateContact, contact: _models.Contact, db: "Session"
+) -> _schemas.Contact:
+    contact.first_name = contact_data.first_name
+    contact.last_name = contact_data.last_name
+    contact.email = contact_data.email
+    contact.phone_number = contact_data.phone_number
+
+    db.commit()
+    db.refresh(contact)
+
     return _schemas.Contact.from_orm(contact)
